62f35ba3ce724d5a0fc4c8de89bbf4ec3f02bc80
Nicholas Butterworth <nicholas.butterworth@gmail.com>
2015-04-23 16:52:14 -0400

    added process.env for port

--- server.js
+++ server.js
@@ -58,8 +58,10 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(9999, function(){
-  console.log('listening on *:9999');
+var port = process.env.PORT || 5000;
+
+http.listen(port, function(){
+  console.log('listening on *: ', port);
 });
 
 
